1a81ddc4c95df97993c7680f1a929d8aa32d50b4
Hugh Saalmans <hugh.saalmans@iag.com.au>
2018-11-07 18:02:00 +1100

    changed directories and added WIP Geomesa Kafka code

---
+++ Kafka/mac_installer/test_java_code/src/au/com/iag/geo/Main.java
@@ -0,0 +1,37 @@
+package au.com.iag.dataeng;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+//import java.util.Properties;
+
+import org.apache.log4j.BasicConfigurator;
+
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.Producer;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.LongSerializer;
+//import org.apache.kafka.common.serialization.StringSerializer;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BasicConfigurator.configure();
+
+        Map<String, Serializable> parameters = new HashMap<>();
+        parameters.put("kafka.zookeepers", "localhost:2181");
+        parameters.put("kafka.brokers", "localhost:9092");
+
+        try {
+
+            org.geotools.data.DataStore dataStore = org.geotools.data.DataStoreFinder.getDataStore(parameters);
+
+            System.out.println(dataStore);
+
+        } catch (Exception e) {
+            System.out.println("Something screwed up!");
+        }
+    }
+}
---
+++ Kafka/mac_installer/xx-code-scraps.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+
+# stop Zookeeper and Kafka
+$KAFKA_HOME/bin/kafka-server-stop.sh
+$KAFKA_HOME/bin/zookeeper-server-stop.sh
+
+# start zookeeper and kafka
+$KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
+$KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
+
+
+
+TOPIC_NAME=fred
+
+# list topics
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZK_HOSTS --list
+
+# delete topic
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZK_HOSTS --delete --topic $TOPIC_NAME
+
+# create topic
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZK_HOSTS --create --topic $TOPIC_NAME --replication-factor 1 --partitions 3
+
+# add file data to topic (producer)
+cat "/Users/s57405/Downloads/a full drive.txt" | $KAFKA_HOME/bin/kafka-console-producer.sh --broker-list $KAFKA_BROKERS --topic $TOPIC_NAME
+
+# read data from topic (comsumer)
+$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server $KAFKA_BROKERS --topic $TOPIC_NAME --from-beginning --max-messages 10
+
+
+
+
+# word count example
+
+
+# create topics
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZK_HOSTS --create --topic streams-plaintext-input --replication-factor 1 --partitions 3
+$KAFKA_HOME/bin/kafka-topics.sh --zookeeper $ZK_HOSTS --create --topic streams-wordcount-output --replication-factor 1 --partitions 3
+
+# create some data
+echo -e "all streams lead to kafka\nhello kafka streams\njoin kafka summit" > ~/tmp/file-input.txt
+
+
+# add data to input topic
+cat "~/tmp/file-input.txt" | $KAFKA_HOME/bin/kafka-console-producer.sh --broker-list $KAFKA_BROKERS --topic streams-plaintext-input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# UNUSED DOCKER KAFKA STUFF
+
+## fire up the kafka shell
+#. docker/start-kafka-shell.sh host.docker.internal host.docker.internal:2181
+
+## create a topic and start a producer
+#$KAFKA_HOME/bin/kafka-topics.sh --create --topic topic --partitions 4 --zookeeper $ZK --replication-factor 2
+#$KAFKA_HOME/bin/kafka-topics.sh --describe --topic topic --zookeeper $ZK
+#$KAFKA_HOME/bin/kafka-console-producer.sh --topic=topic --broker-list='host.docker.internal:32775,host.docker.internal:32774'
+#
+## start a consumer
+#$KAFKA_HOME/bin/kafka-console-consumer.sh --topic=topic --zookeeper=$ZK
+
